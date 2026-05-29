5eb258a366f14a315780831c5fc7ac1d4e410054
Athanasios Diamantis MBP <saki.diamantis@web.de>
2026-05-29 14:09:21 +0200

    initial commit, add Spring Boot 3, add dependencies, add project structure, add .gitignore to root and app level

---
+++ enerflow-app/src/main/java/de/saki/enerflow/EnerflowAppApplication.java
@@ -0,0 +1,13 @@
+package de.saki.enerflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnerflowAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnerflowAppApplication.class, args);
+    }
+
+}
---
+++ enerflow-app/src/test/java/de/saki/enerflow/EnerflowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.saki.enerflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnerflowAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
